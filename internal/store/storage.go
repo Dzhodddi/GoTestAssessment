af1d001a837128927cdf031d0965b1528e549e32
Dzzhodddi <69247957+Dzhodddi@users.noreply.github.com>
2025-06-12 20:56:51 +0300

    feat: add missions and targets storage

--- internal/store/storage.go
+++ internal/store/storage.go
@@ -8,8 +8,9 @@ import (
 )
 
 var (
-	QueryTimeOut = 5 * time.Second
-	ErrNotFound  = errors.New("record not found")
+	QueryTimeOut      = 5 * time.Second
+	ErrNotFound       = errors.New("record not found")
+	MissionedAssigned = errors.New("mission assigned")
 )
 
 type Storage struct {
@@ -20,10 +21,19 @@ type Storage struct {
 		UpdateSpyCat(ctx context.Context, spyCat *Cat) error
 		GetPaginatedSpyCatList(ctx context.Context, paginatedQuery PaginatedQuery) ([]*Cat, error)
 	}
+	Mission interface {
+		CreateMission(ctx context.Context, mission *MissionWithTargets) error
+		DeleteMission(ctx context.Context, id int64) error
+		UpdateMissionStatus(ctx context.Context, mission *UpdatedMission) error
+	}
+	Target interface {
+	}
 }
 
 func NewStorage(db *sql.DB) Storage {
 	return Storage{
-		Cat: &CatStore{db},
+		Cat:     &CatStore{db},
+		Mission: &MissionStore{db},
+		Target:  &TargetStore{db},
 	}
 }
