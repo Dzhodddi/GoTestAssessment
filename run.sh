55dd693a5f84f80628366a83e69a25d798bf8e05
Dzzhodddi <69247957+Dzhodddi@users.noreply.github.com>
2025-06-13 05:02:45 +0300

    fix: fix bash script

--- run.sh
+++ run.sh
@@ -11,6 +11,9 @@ echo "Setting up docker containers..."
 sleep 5
 
 go install -tags 'postgres' github.com/golang-migrate/migrate/v4/cmd/migrate@latest
+go install github.com/swaggo/swag/cmd/swag@latest
+
+swag init -g api/main.go -d cmd,internal && swag fmt
 
 migrate -path internal/db/migrations -database "postgresql://postgres:adminadmin@localhost:5432/testGo?sslmode=disable" up
 
