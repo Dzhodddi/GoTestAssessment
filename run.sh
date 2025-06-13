ae6d117a3a87d502292e2f1e75052543f6796d20
Dzzhodddi <69247957+Dzhodddi@users.noreply.github.com>
2025-06-13 04:39:49 +0300

    add bash script

---
+++ run.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+set -e
+
+mkdir -p test && cd test || exit
+
+git clone https://github.com/Dzhodddi/GoTestAssessment.git .
+
+mkdir -p bin
+go build -o ./bin/main ./cmd/api
+
+docker run -d --name my-postgres -e POSTGRES_USER=postgres -e POSTGRES_PASSWORD=adminadmin -e POSTGRES_DB=testGo -p 5432:5432 postgres:latest
+docker run -d -p 6379:6379 --name my-redis redis:latest
+
+echo "Setting up docker containers..."
+sleep 5
+
+go install -tags 'postgres' github.com/golang-migrate/migrate/v4/cmd/migrate@latest
+
+migrate -path internal/db/migrations -database "postgresql://postgres:adminadmin@localhost:5432/testGo?sslmode=disable" up
+
+./bin/main
