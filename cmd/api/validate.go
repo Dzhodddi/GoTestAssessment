50689de19c9a707fb5b83a86c9e69a80b0097ae1
Dzzhodddi <d.dzhos@ukma.edu.ua>
2025-06-27 13:21:02 +0300

    Add validation for fields in schemas

--- cmd/api/validate.go
+++ cmd/api/validate.go
@@ -2,8 +2,14 @@ package main
 
 import (
 	"WorkAssigment/internal/validation"
+	"context"
+	"github.com/99designs/gqlgen/graphql"
 	"github.com/go-playground/validator/v10"
 )
+
+type ValidatorDirective struct {
+	Validate *validator.Validate
+}
 
 var Validate *validator.Validate
 
@@ -11,4 +17,18 @@ func init() {
 	Validate = validator.New(validator.WithRequiredStructEnabled())
 	catValidator := validation.NewClient("https://api.thecatapi.com/v1/breeds")
 	validation.RegisterCatValidator(Validate, catValidator)
+}
+
+func (v *ValidatorDirective) Binding(ctx context.Context, obj interface{}, next graphql.Resolver, constraint string) (interface{}, error) {
+	val, err := next(ctx)
+	if err != nil {
+		panic(err)
+	}
+
+	err = Validate.Var(val, constraint)
+	if err != nil {
+		return nil, err
+	}
+
+	return val, nil
 }
